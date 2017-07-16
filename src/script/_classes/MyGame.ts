c4d616144118b3b36a514c3d39e5c31462001c48
codeartistic.ninja <the@codeartistic.ninja>
2017-07-16 23:08:08 +0200

    maze

--- src/script/_classes/MyGame.ts
+++ src/script/_classes/MyGame.ts
@@ -14,7 +14,7 @@ class MyGame extends Game {
   constructor(container:string|HTMLElement) {
     super(container, 960);
     this.frameRate = 12;
-    this.addScene("main", new AdventureScene(this, "./assets/maps/4door_room.json"));
+    this.addScene("main", new AdventureScene(this, "./assets/maps/maze.json"));
     this.joypad.enable();
     this.startScene("main");
   }
